69421f220209669a16618015d2db31a7695e4cee
Robert Kerr <robert.kerr@elevationeg.com>
2020-09-13 01:49:48 -0500

    restructure config

--- src/config/twilio.php
+++ src/config/twilio.php
@@ -1,40 +1,46 @@
 <?php
 
 return [
-    'twilio' => [
-        'default' => 'twilio',
-        'connections' => [
-            'twilio' => [
-                /*
-                |--------------------------------------------------------------------------
-                | SID
-                |--------------------------------------------------------------------------
-                |
-                | Your Twilio Account SID #
-                |
-                */
-                'sid' => env('TWILIO_SID', ''),
+    /*
+    |-------------------------------------------------------------------------------
+    | Connections
+    |-------------------------------------------------------------------------------
+    |
+    | The default connection should be enough for most apps. If you need additional
+    | connections you can add them to the configuration here.
+    |
+    */
+    'default' => [
+        /*
+        |--------------------------------------------------------------------------
+        | SID
+        |--------------------------------------------------------------------------
+        |
+        | Your Twilio Account SID #
+        |
+        */
+        'sid' => env('TWILIO_SID', ''),
 
-                /*
-                |--------------------------------------------------------------------------
-                | Access Token
-                |--------------------------------------------------------------------------
-                |
-                | Access token that can be found in your Twilio dashboard
-                |
-                */
-                'token' => env('TWILIO_TOKEN', ''),
+        /*
+        |--------------------------------------------------------------------------
+        | Access Token
+        |--------------------------------------------------------------------------
+        |
+        | Access token that can be found in your Twilio dashboard
+        |
+        */
+        'token' => env('TWILIO_TOKEN', ''),
 
-                /*
-                |--------------------------------------------------------------------------
-                | From Number
-                |--------------------------------------------------------------------------
-                |
-                | The Phone number registered with Twilio that your SMS & Calls will come from
-                |
-                */
-                'from' => env('TWILIO_FROM', ''),
-            ],
-        ],
-    ],
+        /*
+        |--------------------------------------------------------------------------
+        | From Number
+        |--------------------------------------------------------------------------
+        |
+        | The Phone number registered with Twilio that your SMS & Calls will come from,
+        | You can override this when making calls and sms messages by setting the new number
+        | in the $params
+        |
+        */
+        'from' => env('TWILIO_FROM', ''),
+    ]
 ];
