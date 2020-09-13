4bbcd388bcc5828e2b7a21e8b994b9616b3ed925
Robert Kerr <robert.kerr@elevationeg.com>
2020-09-13 01:51:43 -0500

    remove command tests, add start for facade tests

---
+++ tests/Feature/TwilioFacadeTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Aloha\Twilio\Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class TwilioFacadeTest extends TestCase
+{
+    public function it_makes_calls()
+    {
+        $this->markTestIncomplete();
+        $twilio = resolve('twilio');
+    }
+
+    public function it_sends_sms_messages()
+    {
+        $this->markTestIncomplete();
+    }
+}
