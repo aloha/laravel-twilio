7be7925404280cf405bc347ca0ab88f9621691b9
Robert Kerr <robert.kerr@elevationeg.com>
2020-09-13 01:45:46 -0500

    fix config and manager names, clean up container bindings

--- src/ConnectionManager.php
+++ src/ConnectionManager.php
@@ -8,7 +8,7 @@ use Twilio\Rest\Api\V2010\Account\CallInstance;
 use Twilio\Rest\Api\V2010\Account\MessageInstance;
 use Twilio\TwiML\TwiML;
 
-class Manager implements CommunicationsFacilitator
+class Manager
 {
     /* @var string */
     protected $default;
@@ -37,17 +37,6 @@ class Manager implements CommunicationsFacilitator
         $settings = $this->settings[$connection];
 
         return new Twilio($settings['sid'], $settings['token'], $settings['from']);
-    }
-
-    public function message(string $to, string $message, array $mediaUrls = [], array $params = []): MessageInstance
-    {
-        return $this->defaultConnection()->message($to, $message, $mediaUrls, $params);
-    }
-
-    /* @param callable|string|TwiML $message */
-    public function call(string $to, $message, array $params = []): CallInstance
-    {
-        return $this->defaultConnection()->call($to, $message, $params);
     }
 
     public function defaultConnection(): CommunicationsFacilitator
--- src/Support/Laravel/ServiceProvider.php
+++ src/Support/Laravel/ServiceProvider.php
@@ -3,35 +3,30 @@
 namespace Aloha\Twilio\Support\Laravel;
 
 use Aloha\Twilio\Interfaces\CommunicationsFacilitator;
-use Aloha\Twilio\Manager;
+use Aloha\Twilio\ConnectionManager;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class ServiceProvider extends BaseServiceProvider
 {
     public function register()
     {
-        // Register manager for usage with the Facade.
-        $this->app->singleton('twilio', function() {
-            $config = $this->app['config']->get('twilio.twilio');
-
-            return new Manager($config['default'], $config['connections']);
+        $this->app->singleton('twilio-manager', function(Application $app) {
+            $config = $app['config']->get('twilio');
+            return new ConnectionManager($config);
         });
 
-        // Define an alias.
-        $this->app->alias('twilio', Manager::class);
-
-        // Register TwilioInterface concretion.
-        $this->app->singleton(CommunicationsFacilitator::class, function() {
-            return $this->app->make('twilio')->defaultConnection();
+        $this->app->singleton(CommunicationsFacilitator::class, function(Application $app) {
+            return $app->make('twilio-manager')->defaultConnection();
         });
     }
 
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../../config/config.php' => config_path('twilio.php'),
+            __DIR__.'/../../config/twilio.php' => config_path('twilio.php'),
         ], 'config');
 
-        $this->mergeConfigFrom(__DIR__.'/../../config/config.php', 'twilio');
+        $this->mergeConfigFrom(__DIR__.'/../../config/twilio.php', 'twilio');
     }
 }
