5afaddbe6472a939d6c5eef17246885acba6ac07
Robert Kerr <robert.kerr@elevationeg.com>
2020-09-13 14:17:52 -0500

    rename manager

---
+++ src/TwilioManager.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Aloha\Twilio;
+
+use Aloha\Twilio\Interfaces\CommunicationsClient;
+use InvalidArgumentException;
+
+class TwilioManager
+{
+    /* @var string */
+    private $default;
+
+    /* @var array */
+    private $settings;
+
+    public function __construct(string $default = 'twilio', array $settings = [])
+    {
+        $this->default = $default;
+        $this->settings = $settings ?? config('clients');
+    }
+
+    public function from(string $connection): CommunicationsClient
+    {
+        if (empty($this->settings[$connection])) {
+            throw new InvalidArgumentException("Connection \"{$connection}\" is not configured.");
+        }
+
+        $settings = $this->settings[$connection];
+
+        return new Twilio($settings['sid'], $settings['token'], $settings['from']);
+    }
+
+    public function defaultConnection(): CommunicationsClient
+    {
+        return $this->from($this->default);
+    }
+}
