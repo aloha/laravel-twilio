f243630c213eda057083ad313f1233e1d039fadb
Robert Kerr <robert.kerr@elevationeg.com>
2020-09-13 14:15:54 -0500

    rename inteface

---
+++ src/Interfaces/CommunicationsClient.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Aloha\Twilio\Interfaces;
+
+use Twilio\Rest\Api\V2010\Account\CallInstance;
+use Twilio\Rest\Api\V2010\Account\MessageInstance;
+
+interface CommunicationsClient
+{
+    public function call(string $to, $message, array $params): CallInstance;
+
+    public function message(string $to, string $message, array $mediaUrls = [], array $params = []): MessageInstance;
+}
